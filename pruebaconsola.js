233495af2efdeaab0345ec0dbce1834149c621dd
esteve01 <40854268+esteve01@users.noreply.github.com>
2020-07-21 10:16:33 +0200

    Add files via upload

---
+++ pruebaconsola.js
@@ -0,0 +1,15 @@
+window.OPENSHIFT_CONSTANTS.MEMBERSHIP_WHITELIST = [
+  "Admin"
+  "Dev"
+  "View"
+  "admin",
+  "basic-user",
+  "edit",
+  "system:deployer",
+  "system:image-builder",
+  "system:image-puller",
+  "system:image-pusher",
+  "view",
+  "custom-role-1",
+  "custom-role-2"
+];
